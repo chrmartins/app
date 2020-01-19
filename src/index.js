719c895f13215e1ec11ef0597b1072fb696f31c3
Christian Martins <cmts2010@gmail.com>
2020-01-18 21:47:37 -0300

    adição do tema do material ui

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import storeHelper from "./store/store.helper";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import customTheme from "./constants/customTheme";
+
+const theme = createMuiTheme(customTheme);
 
 ReactDOM.render(
   <Provider store={storeHelper}>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
